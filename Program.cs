9420295a0e1bd3532b38c365264ce8a0bdbe01f9
Alexandr <jwchaganav@gmail.com>
2022-11-23 09:19:40 +0400

    testingвваполва

--- Program.cs
+++ Program.cs
@@ -1,53 +1,38 @@
-﻿// Создадим массив через random
-Random rnd = new Random();
-int minR = 2; int maxR = 5;
-int rows = rnd.Next(minR,maxR); int columns = rows; // строки и колонки массива 
-//int columns = rnd.Next(minR, maxR);
-int rowC = 0;
-int[,] matrixA = new int[rows,columns]; // массив A
-int[,] matrixB = new int[columns,rows]; // массив B
+﻿Random rnd = new Random();
+int minR = 1; int maxR = 10;
+int[,,] cube = new int[2,2,2];
 
-int[,] matrixC = new int[rows,columns];
-int minRnd = 1; int maxRnd = 10; // для random
-
-//
-FillRandomArray(matrixA);
-Print2DArray(matrixA);
-
-Console.WriteLine();
-FillRandomArray(matrixB);
-Print2DArray(matrixB);
-
-PowerMatrix(matrixA,matrixB,matrixC);
-//PowerMatrix(test1,test2,test3);
-Console.WriteLine();
-
-Print2DArray(matrixC);
-//Print2DArray(test3);
-
+FillArray(cube);
+Print3DArray(cube);
 
 // Функции 
 
-void FillRandomArray(int[,] array)
+void FillArray(int[,,] array)
 {
-    Random rnd = new Random();
+    int num = 10;
     for(int row = 0; row < array.GetLength(0); row++)
     {
         for(int col = 0; col < array.GetLength(1); col++)
         {
-            array[row,col] = rnd.Next(minRnd,maxRnd);
+            for(int z = 0; z < array.GetLength(2); z++)
+            {
+                num+=rnd.Next(minR,maxR);
+                array[row,col,z] = num;
+            }
         }
     }
 }
 
-void Print2DArray(int[,] array)
+void Print3DArray(int[,,] array)
 {
     for(int i = 0; i < array.GetLength(0); i++)
     {
         for(int j = 0; j < array.GetLength(1); j++)
         {            
-            Console.Write($"{array[i,j]} ");
+            for(int z = 0; z < array.GetLength(2); z++)
+                Console.Write($"{array[i,j,z]}({i},{j},{z}) ");
+            Console.WriteLine();
         }
-        Console.WriteLine();
+        
     }
 }
